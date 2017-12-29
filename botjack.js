099d0ac166798dc82c177761640cb4573fcc30ed
Crasher69 <kinoshnik69@gmail.com>
2017-12-29 21:32:27 +0300

    Update botjack.js

--- botjack.js
+++ botjack.js
@@ -2,7 +2,7 @@
 // @name         МультиКач GUI
 // @description  LW_multi Bot
 // @match        https://www.lowadi.com/*
-// @version      0.4.0
+// @version      0.4.1
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
@@ -759,12 +759,7 @@ function Train1() //докачка треней на родах
 	{
 		ORProg();
 	}
-	else if (getMyParameterByName(walk2 + 'Complet')<95)//65
-	{
-		HayToGive = 14;
-		OatsToGive = 12;
-		WalksBirth(walk2);
-	}
+
 	else if (getMyParameterByName('dressage' + 'TComplet')<100){
 		OatsToGive = 12;
 		HayToGive = -1;
@@ -3016,7 +3011,7 @@ function CompNoVIP_Second(){
 $('body').append('<div class="lwm_logo" style="display:block; z-index:9999; position:fixed; right:0;  top:120px; width:150px;"><img src="https://raw.githubusercontent.com/Crasher69/lowadi/master/horse.png" /></div>');
 
 $('body').append('<style>.lw {color:#fff;}</style><div class="lw_gui" style="display:block; z-index:9999; position:fixed; width:240px; height:auto; right:0; top:300px; padding:5px; background-color: rgba(0, 0, 0, 0.65);  border-radius: 10px 0px 0px 10px;"></div>');
-$('.lw_gui').append('<center><p style="color:#fff;"><span style="font-family: Arial,Helvetica,sans-serif; font-size: 12px; color:#F1F9F1;"><b>BotJack 0.4.0</b></span></p> </center>');
+$('.lw_gui').append('<center><p style="color:#fff;"><span style="font-family: Arial,Helvetica,sans-serif; font-size: 12px; color:#F1F9F1;"><b>BotJack 0.4.1</b></span></p> </center>');
 $('.lw_gui').append('<center><p style="color:#fff;"><span style="font-family: Arial,Helvetica,sans-serif; font-size: 11px; color:#F1F9F1;">  </p></center> <hr>');
 
 $('.lw_gui').append('<span class="lw">id завода:</span> <input type="text" name="lw_zavod" id="lw_zavod" size="18"><br>');
